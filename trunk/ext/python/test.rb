f88afc91c7240daea5284099bb90877f7817577a
Todd Fisher <taf2@bigbox.localdomain>
2008-02-28 21:10:36 -0500

    adding a benchmark test

--- trunk/ext/python/test.rb
+++ trunk/ext/python/test.rb
@@ -25,4 +25,41 @@ class TestPython < Test::Unit::TestCase
     Py.stop
   end
 
+
+  def test_benchmark
+    Py.start
+
+    py_result = ""
+    iterations = 100
+    real_times = []
+
+    iterations.times do
+      timer = Time.now
+      py_result = Py.run( %Q(_rpython_result = {'hello':1,'world':2}), :serialize => 'yaml')
+      real_times << Time.now - timer
+    end
+    total = 0
+    average = 0.0
+    n = 0
+    variance = 0.0
+    real_times.each do|time|
+      n = n + 1
+      delta = time - average
+      average = average + (delta/n)
+      variance = variance + delta * (time - average)
+      total += time 
+    end
+
+    variance /= n 
+
+    puts "total: #{total} seconds, average: #{average} seconds, variance: #{variance} seconds"
+
+    result = YAML.load( py_result )
+    assert_equal 1, result['hello']
+    assert_equal 2, result['world']
+
+
+    Py.stop
+  end
+
 end
